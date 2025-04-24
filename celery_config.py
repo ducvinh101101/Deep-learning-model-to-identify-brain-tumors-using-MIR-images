97b5cb71e3b871993a4292c82746223622779f65
Đức Vinh <vinh221231051@lms.utc.edu.vn>
2025-04-24 21:14:59 +0700

    Add BackEnd
    Connect sever to frontend

---
+++ celery_config.py
@@ -0,0 +1,14 @@
+from celery import Celery
+from flask import Flask
+
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend='redis://localhost:6379/0',
+        broker='redis://localhost:6379/0'
+    )
+    celery.conf.update(app.config)
+    return celery
+
+celery = make_celery(Flask(__name__))
